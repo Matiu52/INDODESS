3e5cf81c54ee7a1199e6ac65168bdf367d8ee865
Matius Andreatna <42943767+Matiu52@users.noreply.github.com>
2021-04-22 21:55:52 +0700

    Menghubungkan Aplikasi dengan Firebase
    Menghubungkan aplikasi dengan Firebase dan membuat tampilan login sementara dari aplikasi. Ketika diklik login data dikirim ke Firebase. Serta, membuat model untuk user.

--- app/src/main/java/com/matius/indodess/MainActivity.java
+++ app/src/main/java/com/matius/indodess/MainActivity.java
@@ -1,8 +1,20 @@
 package com.matius.indodess;
 
-import androidx.appcompat.app.AppCompatActivity;
+/*import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,143 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}*/
+
+import android.os.Bundle;
+        import android.text.TextUtils;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.EditText;
+        import android.widget.TextView;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import com.google.firebase.database.DataSnapshot;
+        import com.google.firebase.database.DatabaseError;
+        import com.google.firebase.database.DatabaseReference;
+        import com.google.firebase.database.FirebaseDatabase;
+        import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private EditText inputUsername, inputPassword;
+    private Button btnLogin;
+    private DatabaseReference mFirebaseDatabase;
+
+    private String userId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inputUsername = findViewById(R.id.username);
+        inputPassword = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btn_login);
+
+        FirebaseDatabase mFirebaseInstance = FirebaseDatabase.getInstance();
+
+        // get reference to 'users' node
+        mFirebaseDatabase = mFirebaseInstance.getReference("users");
+
+        // store app title to 'app_title' node
+        mFirebaseInstance.getReference("app_title").setValue("INDODESS");
+
+        // app_title change listener
+        mFirebaseInstance.getReference("app_title").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.e(TAG, "App title updated");
+
+                String appTitle = dataSnapshot.getValue(String.class);
+
+                // update toolbar title
+                getSupportActionBar().setTitle(appTitle);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read app title value.", error.toException());
+            }
+        });
+
+        // Save / update the user
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = inputUsername.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                // Check for already existed userId
+                if (TextUtils.isEmpty(userId)) {
+                    createUser(username, password);
+                } else {
+                    updateUser(username, password);
+                }
+            }
+        });
+    }
+
+    /**
+     * Creating new user node under 'users'
+     */
+    private void createUser(String username, String password) {
+        // TODO
+        // In real apps this userId should be fetched
+        // by implementing firebase auth
+        if (TextUtils.isEmpty(userId)) {
+            userId = mFirebaseDatabase.push().getKey();
+        }
+
+        User user = new User(username, password);
+
+        mFirebaseDatabase.child(userId).setValue(user);
+
+        addUserChangeListener();
+    }
+
+    /**
+     * User data change listener
+     */
+    private void addUserChangeListener() {
+        // User data change listener
+        mFirebaseDatabase.child(userId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                // Check for null
+                if (user == null) {
+                    Log.e(TAG, "User data is null!");
+                    return;
+                }
+
+                Log.e(TAG, "User data is changed!" + user.getUsername() + ", " + user.getPassword());
+
+
+                // clear edit text
+                inputUsername.setText("");
+                inputPassword.setText("");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.e(TAG, "Failed to read user", error.toException());
+            }
+        });
+    }
+
+    private void updateUser(String username, String password) {
+        // updating the user via child nodes
+        if (!TextUtils.isEmpty(username))
+            mFirebaseDatabase.child(userId).child("name").setValue(username);
+
+        if (!TextUtils.isEmpty(password))
+            mFirebaseDatabase.child(userId).child("email").setValue(password);
     }
 }
---
+++ app/src/main/java/com/matius/indodess/User.java
@@ -0,0 +1,37 @@
+package com.matius.indodess;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+@IgnoreExtraProperties
+class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
